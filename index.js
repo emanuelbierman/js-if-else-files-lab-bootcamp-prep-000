2f053b11a2002eb02413e67ffc08e074d8be7126
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-10-19 21:10:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var name = "Joe"
 var height = "74"
 if (name === "Emanuel") {
   console.log("You have my right name.")
-} else {
+} else if (height === "70") {
+  console.log("You have my right height.")
+else {
   console.log("Expected Emanuel.")
 }
 
-if (height === "70") {
-  console.log("You have my right height.")
-} else {
-  console.log("Expected 70")
-}
 // Don't worry about this
 module.exports = { name, height
 }
