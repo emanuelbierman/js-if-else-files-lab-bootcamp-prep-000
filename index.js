7f1bdec9f8f2be8cd0dddda96ffa62b554f2041c
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-10-19 21:08:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var name = "Joe"
+var name = "Emanuel"
 var height = "74"
 if (name === "Emanuel") {
   console.log("You have my right name.")
 } else {
+} {
   console.log("Expected Emanuel.")
 }
 
